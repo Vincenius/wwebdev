c449092e6378ddcbc3f908e78cf126614fbe3feb
Vincenius <vincet.will@vaitrade.de>
2020-01-16 09:55:48 +0100

    remove commento

--- components/Head/index.js
+++ components/Head/index.js
@@ -28,7 +28,6 @@ class MyHead extends React.Component {
 
                 { isArticle &&
                     <React.Fragment>
-                        <script async defer src="https://cdn.commento.io/js/commento.js"></script>
                         <link rel="canonical" href={link} />
                         <meta name="twitter:card" content="summary" />
                         <meta name="twitter:site" content={link} />
