4d9e1715e7b050b1ae6b343c683523ab38897b3a
Vincenius <vincet.will@vaitrade.de>
2020-01-27 18:15:32 +0100

    fix signup

--- components/SubscribeForm/index.js
+++ components/SubscribeForm/index.js
@@ -5,7 +5,7 @@ function SubscribeForm({ type }) {
     return (
         <S.Container
             id="mc-embedded-subscribe-form"
-            action="https://arcor.us3.list-manage.com/subscribe/post?u=23a611280baff5c6bd68c83e3&amp;id=8dd07d6ca8"
+            action="https://dev.us3.list-manage.com/subscribe/post?u=23a611280baff5c6bd68c83e3&amp;id=8dd07d6ca8"
             method="post"
             name="mc-embedded-subscribe-form"
             novalidate=""
