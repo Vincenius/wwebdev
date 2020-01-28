f2419762bed4296dba53b414c59f7c201c3486f6
Vincenius <vincet.will@vaitrade.de>
2020-01-28 09:40:45 +0100

    remove _document from sitemap

--- scripts/postExport.js
+++ scripts/postExport.js
@@ -16,14 +16,16 @@ const today = formatDate(new Date());
 const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${Object.keys(pathsObj).map(
-        path => `<url>
-            <loc>https://wweb.dev${path}</loc>
-            <lastmod>${
-            pathsObj[path].lastModified
-                ? formatDate(new Date(pathsObj[path].lastModified))
-                : today
-            }</lastmod>
-        </url>`
+        path => path === '/_document'
+            ? ''
+            : `<url>
+                <loc>https://wweb.dev${path}</loc>
+                <lastmod>${
+                pathsObj[path].lastModified
+                    ? formatDate(new Date(pathsObj[path].lastModified))
+                    : today
+                }</lastmod>
+            </url>`
     )}
 </urlset>`;
 
