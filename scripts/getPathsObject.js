046060580f0137a9abdc01c3c5011703e0460cf5
Vincent <isfcikiller@msn.com>
2020-01-28 08:39:55 +0100

    add sitemap and robots.txt generation

---
+++ scripts/getPathsObject.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+module.exports = () => {
+    const fileObj = {};
+
+    const walkSync = dir => {
+        // Get all files of the current directory & iterate over them
+        const files = fs.readdirSync(dir);
+        files.forEach(file => {
+            // Construct whole file-path & retrieve file's stats
+            const filePath = `${dir}${file}`;
+            const fileStat = fs.statSync(filePath);
+
+            if (fileStat.isDirectory()) {
+                // Recurse one folder deeper
+                walkSync(`${filePath}/`);
+            } else {
+                // Construct this file's pathname excluding the "pages" folder & its extension
+                const cleanFileName = filePath
+                .substr(0, filePath.lastIndexOf("."))
+                .replace("pages/", "");
+
+                // Add this file to `fileObj`
+                fileObj[`/${cleanFileName}`] = {
+                page: `/${cleanFileName}`,
+                lastModified: fileStat.mtime
+                };
+            }
+        });
+    };
+
+    // Start recursion to fill `fileObj`
+    walkSync("pages/");
+
+    return fileObj;
+};
---
+++ scripts/postExport.js
@@ -0,0 +1,31 @@
+const fs = require("fs-extra");
+const getPathsObject = require("./getPathsObject");
+const formatDate = require("./formatDate");
+
+// ROBOTS.txt
+const robotsTxt = `User-agent: *
+Sitemap: https://embiem.me/sitemap_local.xml
+Disallow:`;
+
+fs.writeFileSync("out/robots.txt", robotsTxt);
+console.log("robots.txt saved!");
+
+// SITEMAP.XML
+const pathsObj = getPathsObject();
+const today = formatDate(new Date());
+const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${Object.keys(pathsObj).map(
+        path => `<url>
+            <loc>https://embiem.me${path}</loc>
+            <lastmod>${
+            pathsObj[path].lastModified
+                ? formatDate(new Date(pathsObj[path].lastModified))
+                : today
+            }</lastmod>
+        </url>`
+    )}
+</urlset>`;
+
+fs.writeFileSync("out/sitemap_local.xml", sitemapXml);
+console.log("sitemap_local.xml saved!");
