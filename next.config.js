92cc833064343dd6807c8c45c2696e1b40df231b
Vincent <isfcikiller@msn.com>
2020-02-12 07:13:46 +0100

    text weekly export fix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 // next.config.js
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS()
+module.exports = withCSS({
+    exportPathMap: async function(
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+    ) {
+        return {
+            ...defaultPathMap,
+            '/weekly/1': { page: '/weekly/weekly', query: { pid: '1' } },
+            '/weekly/2': { page: '/weekly/[pid]', query: { pid: '2' } },
+            '/weekly/3': { page: '/weekly/[pid]', query: { pid: '3' } },
+        }
+    },
+})
