4b7d4c174f540090c3332660975782e8f7e44eac
Vincent <isfcikiller@msn.com>
2020-02-12 07:37:03 +0100

    dynamic creation of weekly pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 // next.config.js
 const withCSS = require('@zeit/next-css')
+const { weeklyData } = require('./content/weekly')
+
+const weeklyPages = {}
+for (const w of weeklyData) {
+    weeklyPages[`/weekly/${w.id}`] = { page: '/weekly/[pid]', query: { pid: `${w.id}` } }
+}
+
 module.exports = withCSS({
     exportPathMap: async function(
         defaultPathMap,
@@ -7,18 +14,7 @@ module.exports = withCSS({
     ) {
         return {
             ...defaultPathMap,
-            '/weekly/1': { page: '/weekly/[pid]', query: { pid: '1' } },
-            '/weekly/2': { page: '/weekly/[pid]', query: { pid: '2' } },
-            '/weekly/3': { page: '/weekly/[pid]', query: { pid: '3' } },
-            '/weekly/4': { page: '/weekly/[pid]', query: { pid: '4' } },
-            '/weekly/5': { page: '/weekly/[pid]', query: { pid: '5' } },
-            '/weekly/6': { page: '/weekly/[pid]', query: { pid: '6' } },
-            '/weekly/7': { page: '/weekly/[pid]', query: { pid: '7' } },
-            '/weekly/8': { page: '/weekly/[pid]', query: { pid: '8' } },
-            '/weekly/9': { page: '/weekly/[pid]', query: { pid: '9' } },
-            '/weekly/10': { page: '/weekly/[pid]', query: { pid: '10' } },
-            '/weekly/11': { page: '/weekly/[pid]', query: { pid: '11' } },
-            '/weekly/12': { page: '/weekly/[pid]', query: { pid: '12' } },
+            ...weeklyPages,
         }
     },
 })
