3a611149adab42e2c09f7d0a479c31dfbfbc0782
Vincenius <vincet.will@vaitrade.de>
2020-03-30 07:39:38 +0200

    fix typo and dupicate script import

--- public/analytics.js
+++ public/analytics.js
@@ -7,7 +7,5 @@ if (location.hostname !== "localhost" && location.hostname !== "127.0.0.1") {
         var u="https://analytics.vincentwill.com/";
         _paq.push(['setTrackerUrl', u+'hokuspokusp']);
         _paq.push(['setSiteId', '1']);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'hokuspokusj'; s.parentNode.insertBefore(g,s);
     })();
 }
