d391d70791e303bb4b480effa854b1438f8e1da9
Vincenius <vincet.will@vaitrade.de>
2020-04-04 15:32:19 +0200

    fix last empty links

--- components/ArticlePreview/index.js
+++ components/ArticlePreview/index.js
@@ -39,6 +39,7 @@ const ArticlePreview = ({
         <p>{description}</p>
         <S.ReadMore href={link}>
           { isResource ? 'Open Resource' : 'Read more' }
+          <ui.Screenreader>{headline}</ui.Screenreader>
         </S.ReadMore>
       </div>
     </S.Container>
