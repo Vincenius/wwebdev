ed651d061154a2f9c708390aa5f9d06fba3b859f
Vincenius <vincet.will@vaitrade.de>
2020-04-07 07:52:50 +0200

    improve coffee button styling

--- components/BuyMeACoffeeButton/index.js
+++ components/BuyMeACoffeeButton/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import * as S from './styled'
 
 const BuyMeACoffeeButton = props => (
-    <S.Button class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/wwebdev">
+    <S.CoffeeButton target="_blank" rel="noopener noreferrer" href="https://www.buymeacoffee.com/wwebdev">
         <img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee logo" />
         <span>Buy me a coffee</span>
-    </S.Button>
+    </S.CoffeeButton>
 )
 
 export default BuyMeACoffeeButton
