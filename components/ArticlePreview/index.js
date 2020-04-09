cdc096a86f39c47c9d58eef8e8d79cbb7a7cb5e0
Vincenius <vincet.will@vaitrade.de>
2020-04-09 08:15:39 +0200

    improve responsive images

--- components/ArticlePreview/index.js
+++ components/ArticlePreview/index.js
@@ -20,12 +20,29 @@ const ArticlePreview = ({
         <a href={link}>
           <ui.Screenreader>{headline}</ui.Screenreader>
           <LazyLoad offsetVertical={1000}>
-            <picture>
-              <source srcSet={`https://ik.imagekit.io/wwebdev/tr:w-760/${previewImage}`} media="(max-width: 380px)" />
-              <source srcSet={`https://ik.imagekit.io/wwebdev/tr:w-960/${previewImage}`} media="(max-width: 480px)" />
-              <source srcSet={`https://ik.imagekit.io/wwebdev/tr:w-200/${previewImage}`} media="(max-width: 639px)" />
-              <S.PreviewImage src={`https://ik.imagekit.io/wwebdev/tr:w-400/${previewImage}`} alt={headline} />
-            </picture>
+            <S.PreviewImage
+              sizes={
+                `(max-width: 320px) 320px,
+                (max-width: 380px) 380px,
+                (max-width: 480px) 480px,
+                (max-width: 640px) 180px,
+                200px`
+              }
+              srcSet={
+                `https://ik.imagekit.io/wwebdev/tr:w-100/${previewImage} 100w,
+                  https://ik.imagekit.io/wwebdev/tr:w-200/${previewImage} 200w,
+                  https://ik.imagekit.io/wwebdev/tr:w-320/${previewImage} 320w,
+                  https://ik.imagekit.io/wwebdev/tr:w-380/${previewImage} 380w,
+                  https://ik.imagekit.io/wwebdev/tr:w-400/${previewImage} 400w,
+                  https://ik.imagekit.io/wwebdev/tr:w-480/${previewImage} 480w,
+                  https://ik.imagekit.io/wwebdev/tr:w-640/${previewImage} 640w,
+                  https://ik.imagekit.io/wwebdev/tr:w-760/${previewImage} 760w,
+                  https://ik.imagekit.io/wwebdev/tr:w-960/${previewImage} 960w
+                `
+              }
+              src={`https://ik.imagekit.io/wwebdev/${previewImage}`}
+              alt={headline}
+            />
           </LazyLoad>
         </a>
       }
