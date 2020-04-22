226d05c107776d2005965f4bca1f1f8901b00de1
Vincenius <vincet.will@vaitrade.de>
2020-04-22 07:17:51 +0200

    fix sitemap exclude

--- scripts/postExport.js
+++ scripts/postExport.js
@@ -18,7 +18,7 @@ const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${Object.keys(pathsObj)
         .map(
-            path => path === '/_document'  || path === '/weekly/_app' || path === '/weekly/[pid]'
+            path => path === '/_document'  || path === '/_app' || path === '/weekly/[pid]'
                 ? ''
                 : `<url>
                     <loc>https://wweb.dev${path}</loc>
