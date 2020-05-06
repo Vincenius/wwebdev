2c70777e26fe4fb63311519628b22facf0a2f0f7
Vincenius <vincet.will@vaitrade.de>
2020-05-06 07:54:00 +0200

    add border between email elements

--- scripts/convertWeeklyToEmailHtml.js
+++ scripts/convertWeeklyToEmailHtml.js
@@ -1,24 +1,3 @@
-/*
-			<h1 style="display: block; margin: 0; padding: 0; color: #202020; font-family: Helvetica; font-size: 26px; font-style: normal; font-weight: bold; line-height: 125%; text-align: left;">wweb.dev Weekly #</h1>
-			<br />
-			Here&#39;s the <a href="https://wweb.dev/weekly/11" target="_blank">weekly</a> update from the 5th of February 2020<br />
-			<br />
-			&nbsp;
-			<h2><a href="https://hero-generator.netlify.app/?utm_source=wweb.dev">Hero Generator</a></h2>
-
-			<p><a href="https://hero-generator.netlify.app/?utm_source=wweb.dev"><img alt="Hero Generator" height="360" loading="lazy" src="https://res.cloudinary.com/practicaldev/image/fetch/s--c1iiQK38--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/hero-og.png" style="border: 0px  ; width: 600px; height: 360px; margin: 0px 0px 15px;" width="600" /></a><br />
-			A cool generator for hero sections by Sarah Drasner.</p>
-			&nbsp;
-
-			<div class="styled__Description-sc-1fsoux6-2 bCpkap" wfd-id="89">
-			<p><br />
-			<br />
-			<br />
-			Cheers,<br />
-			Vincent from <a href="https://wweb.dev" target="_blank">wweb.dev</a></p>
-			</div>
-*/
-
 const { weeklyData } = require('../content/weekly')
 
 let markDown = `<h1 style="display: block; margin: 0; padding: 0; color: #202020; font-family: Helvetica; font-size: 26px; font-style: normal; font-weight: bold; line-height: 125%; text-align: center;">wweb.dev Weekly #${weeklyData[0].id}</h1>
@@ -33,6 +12,7 @@ for (const item of weeklyData[0].items) {
 
   markDown = `${markDown}
 <br />
+<hr style="border: 1px solid #e0e0e0" />
 <br />
 
 <h2><a href="${item.link}?utm_source=wweb.dev">${item.title}</a></h2>
