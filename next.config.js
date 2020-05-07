82ddcfaab3bfeb3c1f009fe4abe48827aa345132
Vincenius <vincet.will@vaitrade.de>
2020-05-07 08:30:54 +0200

    add lato font locally

--- next.config.js
+++ next.config.js
@@ -23,4 +23,16 @@ module.exports = withCSS({
 
         return pages
     },
+    webpack (config, options) {
+        config.module.rules.push({
+            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                limit: 100000
+                }
+            }
+        })
+        return config
+    }
 })
