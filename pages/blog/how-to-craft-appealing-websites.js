474762c7cea48dd5b7f86bd16daf9284d7ef39ff
Vincenius <vincet.will@vaitrade.de>
2020-05-25 07:41:25 +0200

    fixing grammar and improving text

--- pages/blog/how-to-craft-appealing-websites.js
+++ pages/blog/how-to-craft-appealing-websites.js
@@ -18,15 +18,15 @@ const Post = () => (
   >
     <ui.ArticleContainer as="article">
       <p>
-        When I got started in web-development, I never knew where to start when creating a new website.
-        I was overwhelmed by possibilities, as if I was looking at a blank canvas and didn't know what to draw.
+        When I got started in web development, I never knew where to start when creating a new website.
+        I was overwhelmed by possibilities as if I was looking at a blank canvas and didn't know what to draw.
       </p>
       <p>
-        Over time I came up with a process that helped me not only getting started, but also making the websites
-        more user-friendly and better looking.
+        Over time I came up with a process that helped me not only getting started. It also helped to make
+        the websites more user-friendly and better looking.
       </p>
       <p>
-        In this post I want to share this process.
+        In this post, I want to share this process.
       </p>
 
       <h2>The Preparation</h2>
@@ -35,16 +35,16 @@ const Post = () => (
         just dive into coding right away.
       </p>
       <p>
-        Then I start to think of the purpose of website. Do I want to inform the visitor about something?
+        Then I start to think of the purpose of the website. Do I want to inform the visitor about something?
         Do I want them to sign up on some newsletter or is it something totally different?
       </p>
       <p>
         Now having this in mind, I'll think of what people most probably visit this site for. This can be
-        things like <b>business hours</b>, <b>pricing</b>, <b>follow-up links</b> or any other kind of <b>information</b>.
+        things like <b>business hours</b>, <b>pricing</b>, <b>follow-up links</b>, or any other kind of <b>information</b>.
       </p>
       <p>
         Afterward, I write down what I want to say on my website. This doesn't have to be the final version of your texts.
-        Normally I spend time at the end to polish the copywriting.
+        Normally I spend time in the end to polish copywriting.
       </p>
       <p>
         The most important part here is to bring your content into a structure that makes sense.
@@ -60,13 +60,13 @@ const Post = () => (
         out <a href="https://wweb.dev/resources/websites-for-inspiration" target="_blank" rel="noopener noreferrer">other websites for inspiration</a>.
       </p>
       <p>
-        I don't look at the whole website for inspiration, but at specific structures I came up with when I wrote
+        I don't look at the whole website for inspiration. Instead I look at the specific structures, I came up with when I wrote
         down the content. This way I don't force my content into some template, which doesn't really match.
         This could look like this:
       </p>
 
       <img
-        alt="example on how a list get's transfered into a design"
+        alt="example of how a list gets transformed into a design"
         src="/blog/craft-websites-example.jpg"
       />
 
@@ -75,8 +75,8 @@ const Post = () => (
         or that I've found on my research.
       </p>
       <p>
-        Afterward, it depends on the scope of the project if I convert this drawing into a proper design by
-        using design tools like Figma, Sketch or Adobe XD. Usually I skip this step as I mostly work on small
+        Afterwards, it depends on the scope of the project if I convert this drawing into a proper design by
+        using design tools like Figma, Sketch, or Adobe XD. Usually I skip this step as I mostly work on small
         private projects.
       </p>
 
@@ -84,20 +84,20 @@ const Post = () => (
 
       <p>Then the (for me) fun part can begin: coding the website.</p>
       <p>
-        First of all I decide if I want to go with a design library or want to create everything from scratch, depending
-        on the design / draft, created in the previous step.
+        First of all I decide if I want to go with a design library or want to create everything from scratch.
+        This depends on the design/draft, created in the previous step.
       </p>
       <p>
         I rarely finish the whole website in one take. That's why I always leave <i>TODOs</i> and refactoring
         comments in the code or in the README, while I'm in the flow of coding.
       </p>
       <p>
-        This helps getting back into the project when you don't feel motivated.
+        This helps to get back into the project when you don't feel motivated.
         Just pick up a small refactoring instead and you'll often
-        end up coding more than you orignally planned to.
+        end up coding more than you originally planned to.
       </p>
       <p>
-        I normally start with the desktop version and do the mobile version afterwards.
+        I normally start with the desktop version and do the mobile version afterward.
         But that's just my personal preference. Just do what feels more comfortable for you.
       </p>
 
