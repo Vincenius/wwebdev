214d1703bdd514cb1cea24a31fd0c07dc4aa2517
Vincenius <vincet.will@vaitrade.de>
2020-05-27 08:57:55 +0200

    init search functionality

--- pages/search.js
+++ pages/search.js
@@ -1,19 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { TextField, Button, CircularProgress } from '@material-ui/core'
+
 import * as ui from '../ui'
-import { Layout, SearchInput } from '../components'
+import { Layout } from '../components'
+import searchFunc from '../utils/search'
+
+const Search = () => {
+  const [searchVal, setSearchVal] = useState('')
+  const [state, setState] = useState()
+  const [result, setResult] = useState()
+
+  const startSearch = async () => {
+    setState('loading')
+    const result = await searchFunc(searchVal)
+    setResult(result)
+    setState('done')
+  }
 
 
-const Search = () => (
-  <Layout
-    title="Search"
-  >
-    <ui.Container>
-      <ui.SectionHeadline>Search</ui.SectionHeadline>
+  return (
+    <Layout
+      title="Search"
+    >
+      <ui.Container>
+        <ui.SectionHeadline>Search</ui.SectionHeadline>
 
-      <SearchInput />
-    </ui.Container>
-  </Layout>
-)
+        <TextField
+          required
+          id="filled-required"
+          placeholder="Type here..."
+          variant="filled"
+          value={searchVal}
+          onChange={e => setSearchVal(e.target.value)}
+        />
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={startSearch}
+        >
+          Search
+        </Button>
+
+        { state === 'loading' &&
+          <div><CircularProgress /></div>
+        }
+
+        { state === 'done' && result.length &&
+          <div>Result</div>
+        }
+
+        { state === 'done' && !result.length &&
+          <div>No result</div>
+        }
+      </ui.Container>
+    </Layout>
+  )
+}
 
 export default Search
