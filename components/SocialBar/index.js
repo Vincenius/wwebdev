1244b7e3fac81c82cfc6f32e6278f24d39cc9260
Vincenius <vincent.will@vaitrade.de>
2022-01-28 12:18:15 +0100

    remove facebook link

--- components/SocialBar/index.js
+++ components/SocialBar/index.js
@@ -3,7 +3,6 @@ import * as S from './styled'
 import * as ui from '../../ui'
 import RssFeedIcon from '@material-ui/icons/RssFeed'
 import TwitterIcon from '@material-ui/icons/Twitter'
-import FacebookIcon from '@material-ui/icons/Facebook'
 import GitHubIcon from '@material-ui/icons/GitHub'
 
 const SocialBar = ({ children }) => (
@@ -12,12 +11,6 @@ const SocialBar = ({ children }) => (
             <a href="https://twitter.com/wweb_dev" target="_blank" rel="noopener">
                 <ui.Screenreader>Twitter</ui.Screenreader>
                 <TwitterIcon width="24px" height="24px" />
-            </a>
-        </li>
-        <li>
-            <a href="https://www.facebook.com/wweb.dev.blog" target="_blank" rel="noopener">
-                <ui.Screenreader>Facebook</ui.Screenreader>
-                <FacebookIcon width="24px" height="24px" />
             </a>
         </li>
         <li>
