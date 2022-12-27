c99eea6ab812e9ba43b943116348e678852ff361
Vincenius <vincent.will@vaitrade.de>
2022-12-27 11:43:34 +0100

    affiliate design improvements

--- components/Ads/AffiliateAd.js
+++ components/Ads/AffiliateAd.js
@@ -1,21 +1,26 @@
 import React from 'react'
+import styles from './AffiliateAd.module.css'
 
 export const affiliates = [{
     link: 'https://links.wweb.dev/M7vHb',
     img: '/affiliate/tower.png',
-    text: 'Git Tower - the most powerful Git client for Mac and Windows.'
+    title: 'Git Tower',
+    text: 'the most powerful Git client for Mac and Windows.'
   }, {
     link: 'https://links.wweb.dev/qgMNX',
     img: '/affiliate/refind.png',
-    text: 'Refind - picks 5 links every day that make you smarter, tailored to your interests.'
+    title: 'Refind',
+    text: 'picks 5 links every day that make you smarter, tailored to your interests.'
   }, {
     link: 'https://links.wweb.dev/UbnpO',
     img: '/affiliate/lambdatest.png',
-    text: 'Lambdatest - test across 3000+ different browsers, real devices & operating systems.'
+    title: 'Lambdatest',
+    text: 'test across 3000+ different browsers, real devices & operating systems.'
   }, {
     link: 'https://links.wweb.dev/ZV0Wd',
     img: '/affiliate/digitalocean.jpg',
-    text: 'Digital Ocean - the simple cloud. Sign up now and get 200$ in credit.'
+    title: 'Digital Ocean',
+    text: 'the simple cloud. Sign up now and get 200$ in credit.'
   }
 ]
 
@@ -33,11 +38,13 @@ const AffiliateAd = ({ affiliateAd = 0 }) => {
           <img src={affiliate.img} alt={affiliate.text} style={{ maxWidth: '130px', objectFit: 'cover' }} width="130" height="100" border="0" />
         </span>
         <span className="carbon-text">
-          <span>{affiliate.text}</span>
+          <span>
+            <span className={styles.title}>{affiliate.title}</span> - {affiliate.text}
+          </span>
         </span>
       </span>
     </div>
-    <div style={{ fontSize: '0.65em', fontStyle: 'italic', textAlign: 'right', padding: '.5em 1em 0', maxWidth: '320px' }}>Sponsored</div>
+    <div style={{ fontSize: '0.65em', fontStyle: 'italic', textAlign: 'right', padding: '.5em 1em 0', maxWidth: '330px', margin: '0 auto' }}>Sponsored</div>
   </a>
 }
 
