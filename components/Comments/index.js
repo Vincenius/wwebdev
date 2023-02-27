e69327c2b50cb388a84c28a59a26b33270010634
Vincenius <vincent.will@vaitrade.de>
2023-02-27 11:09:37 +0100

    add utterances comments

--- components/Comments/index.js
+++ components/Comments/index.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { Component } from "react";
 
-const Comments = props => (
-    <div id="commento"></div>
-)
+export default class Comments extends Component {
 
-export default Comments
+    constructor(props) {
+        super(props);
+        this.commentBox = React.createRef(); // use ref to create our element
+    }
+
+    componentDidMount() {
+        let scriptEl = document.createElement("script");
+        scriptEl.setAttribute("theme", 'github-light');
+        scriptEl.setAttribute("src", "https://utteranc.es/client.js");
+        scriptEl.setAttribute("crossorigin", "anonymous");
+        scriptEl.setAttribute("async", true);
+        scriptEl.setAttribute("repo", "wwebdev/utterances");
+        scriptEl.setAttribute("issue-term", "url"); // you can change 'url' with other options
+        this.commentBox.current.appendChild(scriptEl);
+    }
+
+    render() {
+        return (
+            <div style={{ width: '100%' }} id="comments">
+                <div ref={this.commentBox} />
+            </div>
+        );
+    }
+}
