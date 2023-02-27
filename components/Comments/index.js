8ce602b747d0c64408ec1ffbd347aae9a1c51011
Vincenius <vincent.will@vaitrade.de>
2023-02-27 11:47:09 +0100

    use og title for utterances

--- components/Comments/index.js
+++ components/Comments/index.js
@@ -1,28 +1,28 @@
-import React, { Component } from "react";
+import React, { useRef, useEffect } from 'react'
 
-export default class Comments extends Component {
-
-    constructor(props) {
-        super(props);
-        this.commentBox = React.createRef(); // use ref to create our element
-    }
-
-    componentDidMount() {
-        let scriptEl = document.createElement("script");
-        scriptEl.setAttribute("theme", 'github-light');
-        scriptEl.setAttribute("src", "https://utteranc.es/client.js");
-        scriptEl.setAttribute("crossorigin", "anonymous");
-        scriptEl.setAttribute("async", true);
-        scriptEl.setAttribute("repo", "wwebdev/utterances");
-        scriptEl.setAttribute("issue-term", "url"); // you can change 'url' with other options
-        this.commentBox.current.appendChild(scriptEl);
-    }
-
-    render() {
-        return (
-            <div style={{ width: '100%' }} id="comments">
-                <div ref={this.commentBox} />
-            </div>
-        );
-    }
+function Comments() {
+    const ref = useRef(null);
+    useEffect(() => {
+      const scriptElement = document.createElement('script');
+      scriptElement.async = true;
+      scriptElement.crossOrigin = 'anonymous';
+      scriptElement.src = 'https://utteranc.es/client.js';
+  
+      scriptElement.setAttribute('issue-term', 'og:title');
+      scriptElement.setAttribute('label', 'comment');
+      scriptElement.setAttribute(
+        'repo',
+        'wwebdev/utterances',
+      );
+      scriptElement.setAttribute(
+        'theme',
+        'preferred-color-scheme',
+      );
+  
+      ref.current?.appendChild(scriptElement);
+    }, []);
+  
+    return <div ref={ref} />;
 }
+
+export default Comments
