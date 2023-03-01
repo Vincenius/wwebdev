064a0d9c452cbd8e482e9a2dfc511b89fe798287
Vincenius <vincent.will@vaitrade.de>
2023-03-01 07:30:03 +0100

    updates on loader generator

--- components/LoaderGenerator/index.js
+++ components/LoaderGenerator/index.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SketchPicker from 'react-color/lib/Sketch'
+import FormLabel from '@material-ui/core/FormLabel'
+import Slider from '@material-ui/core/Slider'
 import * as S from './styled'
 
 // https://codepen.io/t_afif/pen/PoJyaNy
@@ -6,14 +9,34 @@ import * as S from './styled'
 // https://mui.com/material-ui/react-progress/#circular
 
 const LoaderGenerator = () => {
+  const [settings, setSettings] = useState({
+    color: 'blue',
+    count: 10,
+  })
+  console.log(settings)
   return <div>
     <S.Container>
       <S.LoaderContainer>
-        <S.Loader />
+        <S.Loader settings={settings} />
       </S.LoaderContainer>
 
       <div>
-        yoyo
+        <div>
+          <SketchPicker
+            color={ settings.color }
+            onChangeComplete={color => setSettings({ ...settings, color: color.hex })}
+          />
+        </div>
+        <FormLabel component="legend">Dash count</FormLabel>
+        <Slider
+          value={settings.count}
+          onChange={(e, value) => setSettings({ ...settings, count: value })}
+          aria-labelledby="breakpoint slider"
+          min={5}
+          max={30}
+          step={1}
+          valueLabelDisplay="auto"
+        />
       </div>
     </S.Container>
 
--- components/LoaderGenerator/styled.js
+++ components/LoaderGenerator/styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import Paper from '@material-ui/core/Paper'
 
 export const Container = styled.div`
@@ -10,11 +10,9 @@ export const LoaderContainer = styled(Paper)`
   padding: 1em;
 `
 
-export const Loader = styled.div`
+export const getLoaderCss = ({ count, color }) => css`
   --b: 10px;  /* border thickness */
-  --n: 10;    /* number of dashes*/
   --g: 10deg; /* gap  between dashes*/
-  --c: red;   /* the color */
 
   @keyframes spin1 {to{transform: rotate(1turn)}}
   @keyframes spin2 {
@@ -35,11 +33,15 @@ export const Loader = styled.div`
   aspect-ratio: 1;
   border-radius: 50%;
   padding: 1px;
-  background: conic-gradient(#0000, var(--c)) content-box;
-  mask: repeating-conic-gradient(#0000 0deg, #000 1deg calc(360deg/var(--n) - var(--g) - 1deg),
-    #0000     calc(360deg/var(--n) - var(--g)) calc(360deg/var(--n))),
+  background: conic-gradient(#0000, ${props => props.settings.color}) content-box;
+  mask: repeating-conic-gradient(#0000 0deg, #000 1deg calc(360deg/${props => props.settings.count} - var(--g) - 1deg),
+    #0000     calc(360deg/${props => props.settings.count} - var(--g)) calc(360deg/${props => props.settings.count})),
     radial-gradient(farthest-side,#0000 calc(98% - var(--b)),#000 calc(100% - var(--b)));
   mask-composite: intersect;
 
-  animation: spin1 1s infinite steps(var(--n));
+  animation: spin1 1s infinite steps(${props => props.settings.count});
+`
+
+export const Loader = styled.div`
+  ${props => getLoaderCss(props)}
 `
