8ea506b25ca4f103c414d7b732751a8229da2fe6
Vincenius <vincent.will@vaitrade.de>
2023-03-06 15:51:57 +0100

    use next image for featured

--- components/Featured/index.js
+++ components/Featured/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import resources from '../../content/resources'
 import templates from '../../content/templates'
 import * as S from './styled'
@@ -13,7 +14,9 @@ const Featured = ({ articleIds = [], templateIds = [], resourceIds = [] }) => {
   return <S.Container>
     { articles.map((a, i) =>
       <S.Article key={`featured-${a.headline}`} margin={i === 1}>
-        <Link href={a.link}><img src={a.previewImage} alt={a.headline}/></Link>
+        <Link href={a.link}>
+          <Image src={a.previewImage} alt={a.headline} height={100} width={200} />
+        </Link>
         <Link href={a.link}><h3>{a.headline}</h3></Link>
       </S.Article>
     )}
