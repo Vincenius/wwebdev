2a830b8439bbc428542c6929101def1840e91264
Vincenius <vincent.will@vaitrade.de>
2023-03-10 07:12:59 +0100

    use esbuild-wsm

--- esbuild.js
+++ esbuild.js
@@ -3,8 +3,8 @@ console.log('test');
 require("esbuild")
   .build({
     logLevel: "info",
-    entryPoints: ["src/index.js"],
+    entryPoints: ["./.next/standalone/**/*.js"],
     bundle: true,
-    outfile: "www/main.js",
+    outfile: ".next/standalone",
   })
   .catch(() => process.exit(1));
