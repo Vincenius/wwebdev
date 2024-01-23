ecadffb947dfe4c57b66ed9e52713db67a0d2c5a
Vincenius <vincent.will@vaitrade.de>
2024-01-23 16:13:51 +0100

    replace ads with htmlrev sponsorship

--- components/Ads/Ad.js
+++ components/Ads/Ad.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import AffiliateAd, { affiliates } from './AffiliateAd'
+import HtmlRev from './HtmlRev'
+
 // https://ethical-ad-client.readthedocs.io/en/latest/
 
 const Ad = () => {
-  const [affiliateAd, setAffiliateAd] = useState(null)
+  // const [affiliateAd, setAffiliateAd] = useState(null)
 
-  useEffect(() => {
-    if (window.ethicalads) {
-      window.ethicalads.load()
-    } else {
-      const random = Math.floor(Math.random() * affiliates.length)
-      setAffiliateAd(random)
-    }
-  }, [])
+  // useEffect(() => {
+  //   if (window.ethicalads) {
+  //     window.ethicalads.load()
+  //   } else {
+  //     const random = Math.floor(Math.random() * affiliates.length)
+  //     setAffiliateAd(random)
+  //   }
+  // }, [])
 
-  return affiliateAd === null
-    ? <div className="bordered horizontal adaptive" data-ea-publisher="wwebdev" data-ea-type="image" data-ea-manual="true"></div>
-    : <AffiliateAd affiliateAd={affiliateAd}/>
+  return <HtmlRev />
+
+  // return affiliateAd === null
+  //   ? <div className="bordered horizontal adaptive" data-ea-publisher="wwebdev" data-ea-type="image" data-ea-manual="true"></div>
+  //   : <AffiliateAd affiliateAd={affiliateAd}/>
 }
 
 export default Ad
---
+++ components/Ads/HtmlRev.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './AffiliateAd.module.css'
+
+const AffiliateAd = () => {
+  return <a href="https://htmlrev.com" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', marginBottom: '1em', display: 'block' }}>
+    <div id="carbon">
+      <span className="carbon-wrap">
+        <span className="carbon-img">
+          <img src="/affiliate/htmlrev.png" alt="1000+ free HTML templates for web developers" style={{ maxWidth: '130px', objectFit: 'cover' }} width="130" height="100" border="0" />
+        </span>
+        <span className="carbon-text">
+          <span>
+            <span className={styles.title}>HTMLrev</span> - 1000+ free HTML templates for web developers
+          </span>
+        </span>
+      </span>
+    </div>
+    <div style={{ fontSize: '0.65em', fontStyle: 'italic', textAlign: 'right', padding: '.5em 1em 0', maxWidth: '330px', margin: '0 auto' }}>Sponsored</div>
+  </a>
+}
+
+export default AffiliateAd
